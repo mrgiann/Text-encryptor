3a3e6d3778a545425770162a48d154c5248c3697
Renzo Roggiapane <82038942+mrgiann@users.noreply.github.com>
2023-08-06 10:35:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,32 @@
-// Creamos función encriptar
 function encriptar() {
     let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let mono = document.getElementById("mono");
+
     // Creamos variables de reemplazos
     let textoCifrado = texto
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
+      .replace(/e/gi, "M4j2pS")
+      .replace(/i/gi, "9gJbn0x")
+      .replace(/a/gi, "l56fdGb")
+      .replace(/o/gi, "7b3GH8z")
+      .replace(/u/gi, "8FbS5gkk")
 
     // Si hay texto, ciframos
     if (texto.length !== 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto encriptado con éxito";
-        parrafo.textContent = "";
-
-        // Mostrar alerta de éxito
-        Swal.fire({
-            icon: 'success',
-            title: '¡Texto encriptado con éxito!',
-            showConfirmButton: false,
-            timer: 1500
-        });
-    } else {
-        mono.src = "./img/mono.png";
-        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-
-        // Mostrar alerta de error
-        Swal.fire({
-            icon: 'error',
-            title: 'Oh No!',
-            text: 'No has ingresado el texto a cifrar'
-        });
+      document.getElementById("texto").value = textoCifrado;
     }
-}
+  }
 
-// Si hay texto, desencriptamos
-function desencriptar() {
+  // Si hay texto, desencriptamos
+  function desencriptar() {
     let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let mono = document.getElementById("mono");
 
     let textoCifrado = texto
-        .replace(/enter/gi, "e")
-        .replace(/imes/gi, "i")
-        .replace(/ai/gi, "a")
-        .replace(/ober/gi, "o")
-        .replace(/ufat/gi, "u");
+      .replace(/M4j2pS/gi, "e")
+      .replace(/9gJbn0x/gi, "i")
+      .replace(/l56fdGb/gi, "a")
+      .replace(/7b3GH8z/gi, "o")
+      .replace(/8FbS5gkk/gi, "u")
 
     if (texto.length !== 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto desencriptado con éxito";
-        parrafo.textContent = "";
-
-        // Mostrar alerta de éxito
-        Swal.fire({
-            icon: 'success',
-            title: '¡Texto desencriptado con éxito!',
-            showConfirmButton: false,
-            timer: 1500
-        });
-    } else {
-        mono.src = "./img/mono.png";
-        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-
-        // Mostrar alerta de error
-        Swal.fire({
-            icon: 'error',
-            title: 'Oh No!',
-            text: 'No has ingresado el texto a descifrar'
-        });
+      document.getElementById("texto").value = textoCifrado;
     }
-}
+  }
